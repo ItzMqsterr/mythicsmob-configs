b861cb62b40a9e6dad85905f92ccf12bf75eaf6e
Notkyx <godgamers3566@gmail.com>
2024-12-19 19:31:49 -0700

    Aggiornate script moveconfigs

--- moveconfigs.sh
+++ moveconfigs.sh
@@ -4,7 +4,10 @@ REPO_PATH="/home/mythicsmob-configs/"
 SERVER_PATH="/srv/minecraft/plugins/MythicMobs/"
 
 cd $REPO_PATH
+git add .
+git commit -m "Pull-1"
 git fetch origin main
+git reset --hard origin/main
 git pull -X theirs origin main
 echo "Repository aggioranta con successo e pronta per copiare i file"
 cp -r $REPO_PATH/* $SERVER_PATH/
